07f82fa319190306a409f934426dd639f2c1570c
9seconds <nineseconds@yandex.ru>
2015-12-07 10:34:55 +0300

    Add support for gevent

--- fuelpdsh/__init__.py
+++ fuelpdsh/__init__.py
@@ -3,6 +3,13 @@
 
 import logging
 
+try:
+    import gevent.monkey
+except ImportError:
+    pass
+else:
+    gevent.monkey.patch_all()
+
 
 def logger(name):
     return logging.getLogger("fuelpdsh." + name)
